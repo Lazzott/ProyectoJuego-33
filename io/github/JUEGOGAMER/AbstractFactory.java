31328e3ef269de0fdf467a339a9591d1ce7b4218
Vxrmillion <163214835+Lazzott@users.noreply.github.com>
2024-11-25 06:52:14 -0300

    Delete io/github/JUEGOGAMER directory

--- io/github/JUEGOGAMER/AbstractFactory.java
+++
@@ -1,9 +0,0 @@
-package io.github.JUEGOGAMER;
-
-// La interfaz AbstractFactory define métodos para crear objetos de diferentes tipos
-public interface AbstractFactory {
-    Objetos crearGengar(float x, float y);
-    Objetos crearPokeball(float x, float y);
-    Objetos crearJigglypuff(float x, float y);
-}
-
--- io/github/JUEGOGAMER/EfectoColision.java
+++
@@ -1,7 +0,0 @@
-package io.github.JUEGOGAMER;
-
-public interface EfectoColision {
-    void aplicarEfecto(Entrenador tarro);
-}
-
-
